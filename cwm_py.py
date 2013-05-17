914440635278715386dd4746cb6472579fafefda
Atila Romero <atilaromero@gmail.com>
2013-05-17 11:15:41 -0300

    modified:   cwm_py.py

--- cwm_py.py
+++ cwm_py.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python
 import sys
 sys.path+='.'
-import llyn
-import notation3
 import term
 import __builtin__
-import diag
 
 class BI_Python(term.HeavyBuiltIn,term.ReverseFunction):
-    def __init__(self, resource, fragid):
+    def __init__(self, resource, fragid,func):
         term.Term.__init__(self, resource.store)
         self.resource = resource
         self.fragid = fragid
+        self.pythonCallFunc=func
 
     def evalSubj(self, obj, queue, bindings, proof, query):
         func=self.pythonCallFunc
@@ -20,36 +18,28 @@ class BI_Python(term.HeavyBuiltIn,term.ReverseFunction):
 def newinternFrag(self, fragid, thetype):
     if fragid and self.uri.startswith('python://') and \
            not (self.fragments.has_key(fragid) \
-                and hasattr(self.fragments[fragid],'pythonCallFunc')):
+                and isinstance(self.fragments[fragid],BI_Python)):
         imports=self.uri.split('python://',1)
         if len(imports)>1 and imports[1]:
             module=__import__(imports[1])
             func=getattr(module,fragid)
         else:
             func=getattr(__builtin__,fragid)
-        self.fragments[fragid]=BI_Python(self,fragid)
-        self.fragments[fragid].pythonCallFunc=func
+        self.fragments[fragid]=BI_Python(self,fragid,func)
         return self.fragments[fragid]
     else:
         return self._oldinternFrag(fragid, thetype)
         
 
-# MonkeyPatch
-if not hasattr(term.Symbol,'_oldinternFrag'):
-    term.Symbol._oldinternFrag=term.Symbol.internFrag
-    term.Symbol.internFrag=newinternFrag
+def monkeyPatch(module):
+    if not hasattr(module.Symbol,'_oldinternFrag'):
+        module.Symbol._oldinternFrag=module.Symbol.internFrag
+        module.Symbol.internFrag=newinternFrag
 
-store=llyn.RDFStore()
+monkeyPatch(term)
+sys.modules['swap.term']=term
+sys.modules['swap.swap.term']=term
 
-dest=store.newFormula()
-filt=None
-orig=store.load('teste.n3')
-llyn.applyRules(orig,filt,dest)
-orig.reopen()
-llyn.think(orig)
-outSink=notation3.ToN3(sys.stdout.write)
-#store.dumpNested(orig,outSink)
-store.dumpNested(dest,outSink)
-#relation=store.intern((llyn.SYMBOL,"http://www.w3.org/2000/01/rdf-schema#commentnotEqualIgnoringCase"))
-#match=f.statementsMatching(pred=relation)
-#print 50
+if __name__=='__main__':
+    import cwm
+    cwm.doCommand()
